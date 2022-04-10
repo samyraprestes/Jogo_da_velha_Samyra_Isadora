b77f1683afa01ab1c136e2891b99d85e69e26f4e
samyraprestes <samyraprestesjamilli@gmail.com>
2022-04-10 20:12:56 -0300

    acertos

--- script.js
+++ script.js
@@ -9,6 +9,16 @@ const script = {
     },
     container_element: null,
     gameover: false,
+    acertos: [
+        [0,1,2]
+        [3,4,5]
+        [6,7,8]
+        [0,3,6]
+        [1,4,7]
+        [2,5,8]
+        [0,4,8]
+        [2,4,6]
+    ],
 
     init: function(container){
         this.container_element = container;
@@ -19,8 +29,32 @@ const script = {
         if(this.board[position] === ''){
             this.board[position] = this.simbols.options[this.simbols.turn_index];
             this.draw();
+            let acertos_index = this.check_acertos(this.simbols.options [this.simbols.turn_index]);
+            if(acertos_index >= 0){
+                this.fim_de_jogo();
+            }else{
 
             this.simbols.change();
+            }
+            return true;
+        } else{
+            return false;
+        }
+    },
+
+    fim_de_jogo: function(){
+        this.gameover = true;
+        console.log("FIM DE JOGO");
+    },
+    
+    sequencias_venc: function(simbol){
+        for(i in this.sequencias_venc){
+            if(this.board[this.sequencias_venc[i][0]] == simbol &&
+                this.board[this.sequencias_venc[i][1]] == simbol &&
+                this.board[this.sequencias_venc[i][2]] == simbol ){
+                    console.log('Sequência vencedora: ' + i);
+                    return i;
+                }
         }
     },
 
