90e9dae883f50243b1b605bb99fd8b399b437811
IsaMatosMorais <isadora.morais1797@gmail.com>
2022-04-10 11:54:20 -0300

    script

---
+++ script.js
@@ -0,0 +1,26 @@
+//const script = {
+    //board: ['','','','','','','','','']
+    //simbols: ['X','O']
+    //container_element: null,
+
+    //init: function(container){
+        //this.container_element = container;
+   // }
+
+    //draw: function(){
+        //let content = '';
+
+        //for(i in this,this.board){
+           // content += '<div>' + i + '</div>';
+        ///}
+
+        //this.container_element.innerHTML = content;
+    //}
+
+
+
+
+
+
+
+//};
